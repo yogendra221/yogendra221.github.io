79d5c5b4948777cd5656fdd29e1b88ed33d5e394
Yogendra Kumar V R <kumaryogendra221@gmail.com>
2020-07-21 11:48:04 +0530

    removed some projects

--- project.js
+++ project.js
@@ -10,30 +10,6 @@ var data = {
 		description: "Various Sorting Algorithm Visualization using C++ and OpenGL.",
 		url: "https://github.com/yogendra221/sorting-algorithm-visualizer",
 		language: "C++"
-	},
-	2: {
-		name: "BlogApp",
-		description: "Blog App is a Full Stack web application developed with best applications of RESTful Routing using Node.JS, Express.JS, Embedded JavaScript (EJS) and more.",
-		url: "https://github.com/yogendra221/BlogApp",
-		language: "HTML CSS JavaScript"
-	},
-	3: {
-		name: "ColorGameJS",
-		description: "This a Web Based Game build using HTML CSS JS where user has to identify color based of the RGB values.",
-		url: "https://github.com/yogendra221/ColorGameJS",
-		language: "HTML CSS JavaScript"
-	},
-	4: {
-		name: "Movie-Finder",
-		description: "This is a Web Application that uses oMDB API for searching Movie.",
-		url: "https://github.com/yogendra221/Movie-Finder",
-		language: "HTML JavaScript Bootstrap"
-	},
-	5: {
-		name: "Patatap-Clone",
-		description: "It's a clone of patatap website. It is built with howler.js and paper.js.",
-	url: "https://github.com/yogendra221/Patatap-Clone",
-		language: "HTML CSS JavaScript"
 	}
 };
 
